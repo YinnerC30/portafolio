15a2da2eee3f85845fba623d127ed5047f7843a2
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-02-12 17:02:45 -0500

    feat: 🎸 certifications section

---
+++ src/locales/en/certifications.ts
@@ -0,0 +1,69 @@
+export const certificationsEN = [
+  {
+    title: "Design Patterns",
+    expeditionDate: "February 2025",
+    institution: "Udemy | DevTalles",
+    url: "https://www.udemy.com/certificate/UC-b896f8ca-5281-4ee0-b2f3-ec426df2011b/",
+  },
+  {
+    title: "Cybersecurity",
+    expeditionDate: "October 2024",
+    institution: "Google",
+    url: "https://www.credly.com/badges/9bcd72ba-f5db-4944-b65e-fc8400707cc6/public_url",
+  },
+  {
+    title: "GitHub Foundations",
+    expeditionDate: "August 2024",
+    institution: "GitHub",
+    url: "https://www.credly.com/badges/850776f6-23e3-46c9-b8c1-6d9702c9717a/linked_in_profile",
+  },
+  {
+    title:
+      "Application of the Scrum Framework for Software Development Projects",
+    expeditionDate: "October 2024",
+    institution: "SENA",
+    url: "",
+  },
+  {
+    title: "Software Testing Management",
+    expeditionDate: "October 2024",
+    institution: "SENA",
+    url: "",
+  },
+  {
+    title: "Architecture in Cloud Computing",
+    expeditionDate: "May 2024",
+    institution: "Technological University of Pereira",
+    url: "https://www.acreditta.com/credential/c03828c0-1e3b-4a7e-9b75-b46d637117a1",
+  },
+  {
+    title: "Deployment of Applications in Docker Containers",
+    expeditionDate: "December 2023",
+    institution: "SENA",
+    url: "",
+  },
+  {
+    title: "Data Science and Artificial Intelligence",
+    expeditionDate: "November 2023",
+    institution: "University of Antioquia",
+    url: "",
+  },
+  {
+    title: "Internal Auditor ISO/IEC 27001:2022",
+    expeditionDate: "October 2023",
+    institution: "ES-CIBER",
+    url: "",
+  },
+  {
+    title: "DS4A / Colombia Fundamentals in Data Analytics",
+    expeditionDate: "December 2022",
+    institution: "Correlation One",
+    url: "https://www.credential.net/72557cc9-950f-49fc-b388-d00fb02d3e1b?record_view=true",
+  },
+  {
+    title: "Diploma in Full Stack Web Application Development",
+    expeditionDate: "December 2022",
+    institution: "Technological University of Pereira",
+    url: "",
+  },
+];
---
+++ src/locales/es/certifications.ts
@@ -0,0 +1,71 @@
+export const certificationsES = [
+  {
+    title: "Patrones de diseño",
+    expeditionDate: "febrero de 2025",
+    institution: "Udemy | DevTalles",
+    url: "https://www.udemy.com/certificate/UC-b896f8ca-5281-4ee0-b2f3-ec426df2011b/",
+  },
+  {
+    title: "Ciberseguridad",
+    expeditionDate: "octubre de 2024",
+    institution: "Google",
+    url: "https://www.credly.com/badges/9bcd72ba-f5db-4944-b65e-fc8400707cc6/public_url",
+  },
+
+  {
+    title: "GitHub Fundations",
+    expeditionDate: "agosto de 2024",
+    institution: "GitHub",
+    url: "https://www.credly.com/badges/850776f6-23e3-46c9-b8c1-6d9702c9717a/linked_in_profile",
+  },
+
+  {
+    title:
+      "Aplicación del marco de trabajo scrum para proyectos de desarrollo de software",
+    expeditionDate: "octubre de 2024",
+    institution: "SENA",
+    url: "",
+  },
+  {
+    title: "Manejo de pruebas de software",
+    expeditionDate: "octubre de 2024",
+    institution: "SENA",
+    url: "",
+  },
+  {
+    title: "Architecture in Cloud Computing",
+    expeditionDate: "mayo de 2024",
+    institution: "Universidad Tecnológica de Pereira",
+    url: "https://www.acreditta.com/credential/c03828c0-1e3b-4a7e-9b75-b46d637117a1",
+  },
+  {
+    title: "Despliegue de aplicaciones en contenedores Docker",
+    expeditionDate: "diciembre de 2023",
+    institution: "SENA",
+    url: "",
+  },
+  {
+    title: "Data Science e Inteligencia artificial",
+    expeditionDate: "noviembre de 2023",
+    institution: "Universidad de Antioquia",
+    url: "",
+  },
+  {
+    title: "Auditor Interno ISO/IEC 27001:2022",
+    expeditionDate: "octubre de 2023",
+    institution: "ES-CIBER",
+    url: "",
+  },
+  {
+    title: "DS4A / Colombia Fundamentos en Analítica de Datos",
+    expeditionDate: "diciembre de 2022",
+    institution: "Correlation One",
+    url: "https://www.credential.net/72557cc9-950f-49fc-b388-d00fb02d3e1b?record_view=true",
+  },
+  {
+    title: "Diplomado en Desarrollo de Aplicaciones Web Full Stack",
+    expeditionDate: "diciembre de 2022",
+    institution: "Universidad Tecnológica de Pereira",
+    url: "",
+  },
+];
