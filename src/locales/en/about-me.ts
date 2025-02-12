b6e3326ae8275f7ecd8415715b1e06e8ff95f5b3
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-02-12 17:25:21 -0500

    fix: 🐛 aboutme section

--- src/locales/en/about-me.ts
+++ src/locales/en/about-me.ts
@@ -1,7 +1,4 @@
 export const myInformation = {
   greeting: "Hi, I'm Yinner Chilito",
-  description: `I am an information systems analyst and developer specialized in full stack web applications, with experience in the software development life cycle. 
-        full stack, with experience in the software development life cycle. Such as
-      requirements gathering and definition, design, programming, testing and deployment of the application.
-      testing and deployment of the application.`,
+  description: `I am an information systems analyst and developer specialized in full stack web applications, with experience in the software development life cycle. Such as requirements gathering and definition, design, programming, testing and deployment of the application.`,
 };
