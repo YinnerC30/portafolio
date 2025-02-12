0edf144bb5897800ced9e5e3106ac900efe3faef
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-02-12 15:14:54 -0500

    feat: 🎸 about me section in inglish

---
+++ src/locales/es/about-me.ts
@@ -0,0 +1,7 @@
+export const myInformation = {
+  greeting: "Hola, soy Yinner Chilito",
+  description: `Soy un analista y desarrollador de sistemas de información especializado en aplicaciones web 
+        full stack , con experiencia en el ciclo de vida de desarrollo de software. Tales como
+      la recolección y definición de requerimientos, diseño, programación, realización
+      de pruebas y despliegue del aplicativo.`,
+};
